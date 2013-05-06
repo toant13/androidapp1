a92b1b6e100b2a1d70e70eadc02fade2ee42df03
toantran <toant713@gmail.com>
2013-05-05 18:41:49 -0700

    test committing

--- firstapp/src/com/example/model/LunchList.java
+++ firstapp/src/com/example/model/LunchList.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-
+//test
 
 
 public class LunchList extends Activity
--- firstapp/src/com/example/model/Restaurant.java
+++ firstapp/src/com/example/model/Restaurant.java
@@ -1,6 +1,6 @@
 package com.example.model;
 
-//test
+
 
 public class Restaurant 
 {
