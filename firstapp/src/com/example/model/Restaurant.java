9a2245a1b32565efd7004742995a8f3cde64dfaf
toantran <toant713@gmail.com>
2013-05-05 18:21:07 -0700

    first commit

---
+++ firstapp/src/com/example/model/Restaurant.java
@@ -0,0 +1,31 @@
+package com.example.model;
+
+
+
+public class Restaurant 
+{
+	private String name="";
+	private String address="";
+	
+	
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setAddress(String address)
+	{
+		this.address = address;
+	}
+	
+	public String getAddress()
+	{
+		return address;
+	}
+}
